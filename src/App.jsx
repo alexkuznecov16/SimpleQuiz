68a9ccc3e32ef57a815b045d18b53fd3334a80ba
Alexander Kuznecov <alexander.kuznecov16@gmail.com>
2023-08-18 17:37:12 +0300

    dataConst

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
 import './App.css';
 
 function App() {
-    return <div></div>;
+    const QuizData = [
+        {
+            question: '1 question',
+            variants: ['1', '2', '3', '4'],
+            correct: 0,
+        },
+        {
+            question: '2 question',
+            variants: ['4', '3', '2', '1'],
+            correct: 0,
+        },
+        {
+            question: '3 question',
+            variants: ['1', '2', '3', '4'],
+            correct: 0,
+        },
+        {
+            question: '4 question',
+            variants: ['1', '2', '3', '4'],
+            correct: 0,
+        },
+        {
+            question: '5 question',
+            variants: ['4', '3', '2', '1'],
+            correct: 0,
+        },
+    ];
+    return (
+        <div className='Quiz'>
+            <div className='QuizBlock'>
+                <div className='Progress-bar'>
+                    <div className='Bar'></div>
+                </div>
+                <h1 className='QuestionTitle'></h1>
+            </div>
+        </div>
+    );
 }
 
 export default App;
